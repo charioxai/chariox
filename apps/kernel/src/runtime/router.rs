3c61cec2256e9c8248058626e4072f14c58d185a
miguel <ma.gutierrez.estevez@gmail.com>
2026-05-14 16:20:00 +0200

    Extract router refresh dispatch

--- apps/kernel/src/runtime/router.rs
+++ apps/kernel/src/runtime/router.rs
@@ -10,14 +10,10 @@ use crate::local::{LocalDaemonRequest, LocalDaemonResponse};
 use crate::provider::ProviderRunOperationLanes;
 use crate::runtime::agent_actor::AgentRuntime;
 use crate::runtime::capability_executor::{CapabilityExecutorHealthStore, CapabilityRuntimeStore};
-use crate::runtime::cloud_relay_executor::execute_cloud_relay_request;
-use crate::runtime::command::{KernelCommand, KernelCommandPriority};
+use crate::runtime::command::KernelCommand;
 use crate::runtime::command_response_refresh::{
     refresh_command_response_state, CommandResponseRefreshContext,
 };
-use crate::runtime::history_executor::execute_history_request;
-use crate::runtime::kernel_lifecycle_executor::execute_kernel_lifecycle_request;
-use crate::runtime::pairing_invite_executor::execute_pairing_request;
 use crate::runtime::projection::{
     AgentRuntimeProjectionStore, DaemonConfigProjectionStore, ProviderCatalogProjectionStore,
     ProviderProcessProjectionStore, ProviderRunProjectionStore,
@@ -25,19 +21,13 @@ use crate::runtime::projection::{
     SessionStateProjectionStore, TransportHealthStore,
 };
 use crate::runtime::provider_launch_executor::ProviderLaunchPendingTracker;
-use crate::runtime::provider_process_control::execute_provider_process_request;
-use crate::runtime::relay_config_control::execute_relay_config_request;
-use crate::runtime::remote_machine_registry::execute_remote_machine_registry_request;
 use crate::runtime::session_actor::{FocusedAgentProjection, SessionRuntime};
-use crate::runtime::session_collaboration_executor::execute_session_collaboration_request;
 use crate::runtime::session_membership::authorize_session_membership;
 use crate::runtime::session_projection_refresh::{
     focus_projection_refresh, session_projection_refresh,
 };
-use crate::runtime::slice_command_executor::execute_slice_request;
 use crate::runtime::state::KernelRuntimeState;
 use crate::runtime::terminal_output_executor::TerminalOutputExecutor;
-use crate::runtime::user_config_executor::execute_user_config_request;
 use crate::runtime::workflow_actor::WorkflowRuntime;
 use crate::runtime::workspace_coordinator::WorkspaceCoordinator;
 use crate::terminal::TerminalStreamHealthStore;
@@ -48,6 +38,7 @@ mod cloud_relay_bridge;
 mod composition;
 mod pre_lane_dispatch;
 mod priority_dispatch;
+mod refresh_dispatch;
 mod relay_peer_bridge;
 mod runtime_tool_bridge;
 mod status_projection_bridge;
@@ -160,155 +151,7 @@ impl CommandRouter {
         }
 
         let session_refresh = session_projection_refresh(&request);
-        let result = match request {
-            request @ LocalDaemonRequest::ConfigureRelay(_) => {
-                execute_relay_config_request(
-                    &self.app,
-                    Arc::clone(&self.relay_state),
-                    &self.config_projection,
-                    &self.provider_catalog_projection,
-                    request,
-                )
-                .await
-            }
-            request @ (LocalDaemonRequest::CloudRelayStatus(_)
-            | LocalDaemonRequest::StartCloudRelayLogin(_)
-            | LocalDaemonRequest::PollCloudRelayLogin(_)
-            | LocalDaemonRequest::LogoutCloudRelay(_)
-            | LocalDaemonRequest::PairCloudRelayClient(_)
-            | LocalDaemonRequest::PairCloudRelayMachine(_)
-            | LocalDaemonRequest::ConnectCloudRelay(_)
-            | LocalDaemonRequest::IssueCloudRelayClientToken(_)
-            | LocalDaemonRequest::CreateCloudSessionInvite(_)
-            | LocalDaemonRequest::ShowCloudSessionInvite(_)
-            | LocalDaemonRequest::AcceptCloudSessionInvite(_)
-            | LocalDaemonRequest::RevokeCloudSessionInvite(_)
-            | LocalDaemonRequest::ListCloudSessionMembers(_)
-            | LocalDaemonRequest::ListCloudCollaborators(_)) => {
-                execute_cloud_relay_request(
-                    &self.app,
-                    &self.config_projection,
-                    &self.provider_catalog_projection,
-                    Arc::clone(&self.relay_state),
-                    request,
-                )
-                .await
-            }
-            request @ (LocalDaemonRequest::GetUserConfig(_)
-            | LocalDaemonRequest::GetUserConfigSchema(_)
-            | LocalDaemonRequest::SetUserConfigValue(_)
-            | LocalDaemonRequest::UnsetUserConfigValue(_)
-            | LocalDaemonRequest::SetCredentialSecret(_)
-            | LocalDaemonRequest::DeleteCredentialSecret(_)) => {
-                execute_user_config_request(
-                    &self.app,
-                    &self.config_projection,
-                    &self.runtime_state,
-                    request,
-                )
-                .await
-            }
-            request @ (LocalDaemonRequest::ListSlices(_)
-            | LocalDaemonRequest::CreateSlice(_)
-            | LocalDaemonRequest::GetSlice(_)
-            | LocalDaemonRequest::StartSlice(_)
-            | LocalDaemonRequest::StopSlice(_)
-            | LocalDaemonRequest::DeleteSlice(_)
-            | LocalDaemonRequest::ImportSliceProviderAuth(_)
-            | LocalDaemonRequest::GetSliceDisplayEndpoint(_)) => {
-                execute_slice_request(&self.app, &self.config_projection, request).await
-            }
-            request @ LocalDaemonRequest::DeleteKernel(_) => {
-                execute_kernel_lifecycle_request(
-                    &self.config_projection,
-                    &self.runtime_state,
-                    request,
-                )
-                .await
-            }
-            request @ (LocalDaemonRequest::ApproveRemoteMachine(_)
-            | LocalDaemonRequest::ForgetRemoteMachine(_)
-            | LocalDaemonRequest::RenameRemoteMachine(_)) => {
-                execute_remote_machine_registry_request(
-                    &self.app,
-                    &self.config_projection,
-                    &self.provider_catalog_projection,
-                    request,
-                )
-                .await
-            }
-            request @ (LocalDaemonRequest::ListSessionMembers(_)
-            | LocalDaemonRequest::CreateSessionInvite(_)
-            | LocalDaemonRequest::JoinSessionInvite(_)
-            | LocalDaemonRequest::RevokeSessionInvite(_)
-            | LocalDaemonRequest::CreateWorkspaceLink(_)
-            | LocalDaemonRequest::ListWorkspaceLinks(_)
-            | LocalDaemonRequest::ShowWorkspaceLink(_)
-            | LocalDaemonRequest::AttachWorkspaceLink(_)
-            | LocalDaemonRequest::DetachWorkspaceLink(_)) => {
-                execute_session_collaboration_request(
-                    &self.app,
-                    &self.session_projection,
-                    &self.config_projection,
-                    &command,
-                    request,
-                )
-                .await
-            }
-            request @ (LocalDaemonRequest::CreatePairingInvite(_)
-            | LocalDaemonRequest::JoinPairingInvite(_)
-            | LocalDaemonRequest::CreateTerminalPairingLink(_)
-            | LocalDaemonRequest::JoinTerminalPairingLink(_)
-            | LocalDaemonRequest::ListTerminals(_)
-            | LocalDaemonRequest::ListPairedClients(_)
-            | LocalDaemonRequest::RecordPairedClient(_)
-            | LocalDaemonRequest::RevokePairedClient(_)) => {
-                execute_pairing_request(
-                    &self.app,
-                    &self.config_projection,
-                    &self.provider_catalog_projection,
-                    request,
-                )
-                .await
-            }
-            request @ (LocalDaemonRequest::GetSessionHistory(_)
-            | LocalDaemonRequest::GetPromptInputHistory(_)
-            | LocalDaemonRequest::RecordPromptInputHistory(_)
-            | LocalDaemonRequest::QueryHistory(_)
-            | LocalDaemonRequest::SearchHistory(_)
-            | LocalDaemonRequest::SemanticSearchHistory(_)) => {
-                execute_history_request(
-                    &self.app,
-                    self.history_store.clone(),
-                    self.operational_history_store.clone(),
-                    self.history_projection.clone(),
-                    &self.runtime_state,
-                    &self.config_projection,
-                    request,
-                )
-                .await
-            }
-            LocalDaemonRequest::PumpTerminalOutput(request) => {
-                self.terminal_output_executor.execute(request).await
-            }
-            request @ LocalDaemonRequest::TeardownProviderProcesses(_) => {
-                execute_provider_process_request(
-                    &self.app,
-                    &self.session_projection,
-                    &self.agent_runtime_projection,
-                    request,
-                )
-                .await
-            }
-            request => match command.priority {
-                KernelCommandPriority::Interactive => {
-                    self.dispatch_interactive(command, request).await
-                }
-                KernelCommandPriority::Normal | KernelCommandPriority::Background => {
-                    self.dispatch_normal_or_background(command, request).await
-                }
-            },
-        };
+        let result = self.dispatch_refresh_tracked(command, request).await;
         refresh_command_response_state(
             CommandResponseRefreshContext {
                 app: &self.app,
